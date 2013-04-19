3ad1846b0e587fbf47c305a7991e55013bbb89f0
Jorge Moscat <jorgemoscat@gmail.com>
2013-04-18 18:54:25 -0700

    Added controller and views skeleton

--- app/controllers/cupon_controller.rb
+++ app/controllers/cupon_controller.rb
@@ -1,2 +1,16 @@
 class CuponController < ApplicationController
+
+	def show
+    @cupon = Discount.find(:first, :conditions => [ "hash_key = ?", params[:hash_key]])
+    @redeem = "localhost:3000/redeem/"+@cupon.hash_key
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @redeem }
+    end
+  end
+
+  def redeem
+  	@cupon = Discount.find(:first, :conditions => [ "hash_key = ?", params[:hash_key]])
+  	@client_password = Discount.where(:hash_key => params[:hash_key]).first.client.password
+  end
 end
