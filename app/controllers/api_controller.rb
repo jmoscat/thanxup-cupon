f9d66263de1bd6c0931c95bff608729c28e464d4
Jorge Moscat <jorgemoscat@gmail.com>
2013-05-29 18:18:24 +0200

    Fxes

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,10 @@
 class ApiController < ApplicationController
-
+skip_before_filter :verify_authenticity_token
+#respond = RestClient.post "http://localhost:3000/api/template", {:user_name => "jorge", :venue_name => "paco", :venue_pass => "123" ,:user_id => "123432", :venue_id => "121231", :cupon_text => "Lorem ipsum", :valid_from => "Sat, 18 May 2013 09:17:57 -0700", :valid_until => "Tue, 21 May 2013 09:17:57 -0700", :kind => "SHA", :social_text => "Lorem ipsum social", :social_count => 0, :social_limit => 3, :social_from => "Sat, 18 May 2013 09:17:57 -0700", :social_until => "Tue, 21 May 2013 09:17:57 -0700"}.to_json, :content_type => :json, :accept => :json
 	def template
-		Template.perform_async(params)
-		#Cupon.cupon_from_template(params)
+		#Template.perform_async(params)
+		Cupon.cupon_from_template(params)
+		debugger
 		render :status =>200, :json=> {:status => "Success"}
 	end
 
