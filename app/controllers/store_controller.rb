3e367880e98cd9a1de2fbedd45eb05957f2be9f9
Jorge Moscat <jorgemoscat@gmail.com>
2014-03-29 19:13:56 +0100

    Fixed get app

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,7 @@
 class StoreController < ApplicationController
-  #curl -v -H "Accept: application/json" -H "Content-type: application/json" -X GET http://localhost:3000/getapp.json
+  #curl -v -H "Accept: application/json" -H "Content-type: application/json" -X GET http://192.168.1.103:3000/getapp.json
   def getapp
-    if request.user_agent =~ /iPod|iPad|iPhone|Android/i
+    if request.user_agent =~ /iPod|iPad|iPhone/i
   	  redirect_to "https://itunes.apple.com/uy/app/thanxup/id764549346?mt=8"
     elsif request.user_agent =~ /iAndroid/i
       redirect_to "https://play.google.com/store/apps/details?id=com.thanxup.android.app"
